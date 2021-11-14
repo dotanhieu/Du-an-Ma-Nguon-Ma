0cd51af3644826799a3aa0edd123a10afe279433
dotanhieu <dotanhieu1@gmail.com>
2021-11-14 11:20:43 +0700

    update login

--- login.php
+++ login.php
@@ -21,29 +21,21 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
 				$p_list = stripcslashes($_COOKIE["product_list"]);
 				$product_list = json_decode($p_list,true);
 				for ($i=0; $i < count($product_list); $i++) { 
-					//After getting user id from database here we are checking user cart item if there is already product is listed or not
 					$verify_cart = "SELECT id FROM cart WHERE user_id = $_SESSION[uid] AND p_id = ".$product_list[$i];
 					$result  = mysqli_query($con,$verify_cart);
 					if(mysqli_num_rows($result) < 1){
-						//if user is adding first time product into cart we will update user_id into database table with valid id
 						$update_cart = "UPDATE cart SET user_id = '$_SESSION[uid]' WHERE ip_add = '$ip_add' AND user_id = -1";
 						mysqli_query($con,$update_cart);
 					}else{
-						//if already that product is available into database table we will delete that record
 						$delete_existing_product = "DELETE FROM cart WHERE user_id = -1 AND ip_add = '$ip_add' AND p_id = ".$product_list[$i];
 						mysqli_query($con,$delete_existing_product);
 					}
 				}
-				//here we are destroying user cookie
 				setcookie("product_list","",strtotime("-1 day"),"/");
-				//if user is logging from after cart page we will send cart_login
 				echo "cart_login";
-				
-				
 				exit();
 				
 			}
-			//if user is login from page we will send login_success
 			echo "login_success";
 			$_SESSION["uid"] = $row["user_id"];
 		    $_SESSION["name"] = $row["first_name"];
@@ -51,13 +43,10 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
 				if(!isset($BackToMyPage)) {
 					header('Location: '.$BackToMyPage);
 					echo"<script type='text/javascript'>
-					
 					</script>";
 				} else {
 					header('Location: index.php'); // default page
 				} 
-				
-			
             exit;
 
 		}else{
@@ -67,21 +56,15 @@ if(isset($_POST["email"]) && isset($_POST["password"])){
                 $run_query = mysqli_query($con,$sql);
                 $count = mysqli_num_rows($run_query);
 
-            //if user record is available in database then $count will be equal to 1
+            
             if($count == 1){
                 $row = mysqli_fetch_array($run_query);
                 $_SESSION["uid"] = $row["admin_id"];
                 $_SESSION["name"] = $row["admin_name"];
                 $ip_add = getenv("REMOTE_ADDR");
-                //we have created a cookie in login_form.php page so if that cookie is available means user is not login
-
-
-                    //if user is login from page we will send login_success
                     echo "login_success";
-
                     echo "<script> location.href='admin/addproduct.php'; </script>";
                     exit;
-
                 }else{
                     echo "<span style='color:red;'>Tài khoản hoặc mật khẩu không đúng...!</span>";
 					echo "<span style='color:red;'>Vui lòng đăng kí trước khi đăng nhập...!</span>";
